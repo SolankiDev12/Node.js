e78dce2cac1817e922c8ccd3d1eca1cf9fa2dda0
SolankiDev12 <ddevkumarp123@gmail.com>
2025-07-02 23:27:59 +0530

    JWT_Completed

--- 09_Session_Cookies/01_server.js
+++ 09_Session_Cookies/01_server.js
@@ -7,9 +7,34 @@ const db = require('./02_db')
 
 const { jwtMiddlewareToken , generateToken } = require('./04_jwt')
 
-app.get('/' ,(req,res) => 
+app.get('/' , jwtMiddlewareToken, async(req,res) => 
 {
-    res.send(`<h1> JWT TOKEN</h1>`)
+    // res.send(`<h1> JWT TOKEN</h1>`)
+    try{
+        const data = await AuthSchema.find();
+        console.log('Data fetched');
+        res.status(200).json(data);
+    }catch(err)
+    {
+        console.log(err)
+        res.status(500).json({err : 'Internal Server Error'})
+    }
+})
+app.get('/profile' , jwtMiddlewareToken, async(req,res) =>  //see only your profile
+{
+    try{
+        const userdata = req.userpayload; //we will get userdata like name and pwd bcz of token
+        console.log('User Data : ' ,userdata);
+
+        const userID = userdata.id;
+        const user = await AuthSchema.findById(userID); //search this user based on extracted ID
+
+        res.status(200).json(user);
+    }catch(err)
+    {
+        console.log(err)
+        res.status(500).json({err : 'Internal Server Error'})
+    }
 })
 
 app.post('/signup' , async(req, res) =>{
@@ -38,8 +63,29 @@ app.post('/signup' , async(req, res) =>{
     }
 })
 
-app.get('/signin' , async(req, res) =>{
-   //yet to impelement
+app.post('/signin' , async(req, res) =>{ //give ur username , pwd server will verify on JWT token on basis of  that
+   try{
+    const {username , password} = req.body;
+
+    const user = await AuthSchema.findOne({username: username});
+    if(!user || ! (await user.comparepwd(password)) )
+    {
+        return res.status(500).json({ error  : 'Internal server error '})
+    }
+    //gen token and then compare with orginal one
+    const payload =  {
+        id : user.id , 
+        username : user.username
+    }
+
+    const token = generateToken(payload);
+    res.status(200).json({token : token});
+
+   }catch(err)
+   {
+        console.log(err);
+        res.status(500).json({err : 'Internal server error'})
+   } 
 })
 
 const port = 3000 ;
--- 09_Session_Cookies/04_jwt.js
+++ 09_Session_Cookies/04_jwt.js
@@ -5,14 +5,20 @@ require('dotenv').config()
 //verify the given token
 const jwtMiddlewareToken = (req, res , next) => 
 {
+    //first check req header for authorization
+    const authorization = req.headers.authorization
+    if(!authorization) return res.status(401).json({err : 'Invalid Token or Token not found'})
+
+
     const token = req.headers.authorization.split(' ')[1]
 
     if(!token) //not found
     return res.status(401).json({error : "Unauthorized"})
 
     try{
+        // const decoded = jwt.verify(token,process.env.JWT_SECRET_KEY, {expiresIn : 30}); //timer 30 sec
         const decoded = jwt.verify(token,process.env.JWT_SECRET_KEY); //token and key to verify
-
+        
         req.userpayload = decoded;  //attach user info to request object
         next();
     }catch(err)
@@ -27,7 +33,7 @@ const jwtMiddlewareToken = (req, res , next) =>
 //Function to generate the token
 const generateToken = (userData) => { //it needs payload which we get from userdata
     return jwt.sign(userData, process.env.JWT_SECRET_KEY); //assigns jwt token and for that we give userdata + secret key
-}
+}   //instead of userdata as a string we can pass any object like {username : 'xyz', id : '123'} which we will better
 
 
 
